35540f605147fd83f79c1f50ca0a4d51df0230b1
Soria Alvaro <alvarosoria.26@gmail.com>
2023-11-15 02:48:58 -0300

    agregadologin y register

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-const express = require('express')
-const path = require('path')
-const port = process.env.PORT || 3001;
+const express = require("express");
+const path = require("path");
+const port = 8080;
 
-const app = express()
-app.use(express.static(path.join(__dirname,"public")));
+const app = express();
 
-app.listen(port, () => { 
-    console.log("Escuchando")
+app.listen(port, () => {
+    console.log(`Servidor habilitado http://localhost:${port}`);
+});
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res)=>{
+    res.sendFile(path.resolve("./views/home.html"));
 })
 
-app.get("/", (req,res)=> {
-    res.sendFile(path.resolve("./views/home.html"))
+app.get("/home", (req, res)=>{
+    res.sendFile(path.resolve("./views/home.html"));
 })
 
-app.get("/home", (req,res)=> {
-    res.sendFile(path.resolve("./views/home.html"))
+app.get("/register", (req, res)=>{
+    res.sendFile(path.resolve("./views/register.html"));
 })
-
-
+app.get("/login", (req, res)=>{
+    res.sendFile(path.resolve("./views/login.html"));
+})
